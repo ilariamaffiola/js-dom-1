e270ac923df31d850808887ca28ee1123cd7cd11
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-25 17:19:57 +0100

    [UPD] pagina html e dichiarazione variabili js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//dichiaro le variabili recuoerando quindi gli elementi del DOM
+let button = document.querySelector('button');
+console.log(button);
+let lampadina = document.querySelector('img');
+console.log(lampadina);
+//CORPO DEL PROGRAMMA
+//aggiungo un event listener al bottone
+button.addEventListener('Click', function(){
+    //con il click sul bottone dovrei accendere la lampadina, quindi controllo prima se col click funziona con un console.log
+    console.log('CLICCATO');
+})
--- readme.md
+++ readme.md
@@ -9,5 +9,5 @@ Facciamo accendi e spegni:
 Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
 Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
 E così via...
-Per fare questo bonus potremmo aver bisogno di del metodo string.includes() :occhiolino:
-Buon Lavoro e buon divertimento! :sorridere: (modificato) 
+Per fare questo bonus potremmo aver bisogno di del metodo string.includes() 
+Buon Lavoro e buon divertimento! 
