1312c39a574a2ebc4c2dfa83d5e0948bfbdcaf70
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-25 17:43:04 +0100

    [UPD] ESERCIZIO FINITO implement lampadina toggle functionality on button click

--- js/script.js
+++ js/script.js
@@ -8,5 +8,14 @@ console.log(lampadina);
 button.addEventListener('click', function(){
     //con il click sul bottone dovrei accendere la lampadina, quindi controllo prima se col click funziona con un console.log
     console.log('CLICCATO');
-
+        //una volta accertati che questo funziona, possiamo iniziare a scrivere il codice per accendere la lampadina
+    //per accendere la lampadina dobbiamo cambiare l'attributo src dell'immagine
+    //lampadina.src="./img/yellow_lamp.png";
+    //ora che abbiamo visto che funziona, commento il tutto per trovare il modo di farla amche spegnere
+    if(lampadina.src.includes('white')){
+        lampadina.src="./img/yellow_lamp.png";
+    }
+    else{
+        lampadina.src="./img/white_lamp.png";
+    }
 })
